52c8ca6d229a9e23722241487167c924fcbde0a6
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 19:32:45 +0530

    Initial Commit

---
+++ src/hello_world/Hello.java
@@ -0,0 +1,15 @@
+package hello_world;
+
+/**
+ * The code that breaks(?) your virginity for any programming language.
+ * The afe-old, tried-and-tested Hello World!
+ *
+ * @author Maverick D. Aece (Maverick-D-Aece)
+ * @version 1.0
+ */
+public class Hello {
+
+    public static void main() {
+        System.out.println("Hello World!");
+    }
+}
