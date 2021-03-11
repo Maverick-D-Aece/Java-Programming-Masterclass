ed5cf84c64d571aaecc38f99e54e7cd2c33daa5b
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 20:06:36 +0530

    Adding the Variables.java class.

---
+++ src/variables/Variables.java
@@ -0,0 +1,17 @@
+package variables;
+
+/**
+ * Shows a bunch of primitive variables that we can have in Java
+ * and how we can print them.
+ *
+ * @author Maverick D. Aece
+ * @version 1.0
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        // declares and defines(initializes) an integer variable
+        int number = 7;
+        System.out.println("Value of the \"number\" variable = " + number);
+    }
+}
