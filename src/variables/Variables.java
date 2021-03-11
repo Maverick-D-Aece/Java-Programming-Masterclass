edfd6ab23cc99587093ab8d68b15056ff4e48906
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 20:45:14 +0530

    Added some more variables

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -5,14 +5,19 @@ package variables;
  * and how we can print them.
  * v1.1 shows how we can assign a new value to a variable as an expression.
  * Added a complex expression in v1.2
+ * Added some more variables in v1.3
+ *
  * @author Maverick D. Aece
- * @version 1.2
+ * @version 1.3
  */
 public class Variables {
 
     public static void main(String[] args) {
         // declares and defines(initializes) an integer variable
         int number = (7 + 5)  * (12 - 3);
-        System.out.println("Value of the \"number\" variable = " + number);
+        int anotherNumber = 8;
+        int anotherAnotherNumber = 9;
+        int total = number + anotherNumber + anotherAnotherNumber;
+        System.out.println("Value of the \"total\" variable = " + total);
     }
 }
