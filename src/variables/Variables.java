107e14c5fd910af4199b670f18151a8efa7b75fb
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 21:09:55 +0530

    Busting and Boosting the max and min values of an integer

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -9,17 +9,18 @@ package variables;
  * Assigned one variable to another variable in v1.4
  * Added a final variable that has the value 1000 less than the total, in v1.5
  * Added code to find the range of the "int" data type in v1.6
+ * Busting and Boosting the max and min values respectively for "int" data type in v1.7
  * [One small IntelliJ IDEA hack that I learned today - write "sout" and Ctrl+Space
  * for println template]
  *
  * @author Maverick D. Aece
- * @version 1.6
+ * @version 1.7
  */
 public class Variables {
 
     public static void main(String[] args) {
         // declares and defines(initializes) an integer variable
-        int number = (7 + 5)  * (12 - 3);
+        int number = (7 + 5) * (12 - 3);
         int anotherNumber = 8;
         int anotherAnotherNumber = number - 10;
         int total = number + anotherNumber + anotherAnotherNumber;
@@ -31,5 +32,12 @@ public class Variables {
         int min = Integer.MIN_VALUE;
         int max = Integer.MAX_VALUE;
         System.out.println("\nA variable of \"int\" data type can have values from " + min + " to " + max);
+
+        // trying to add even "1" to max will result in overflow of the value in the variable
+        // in this case, the value stored in max will be the same as that in min
+        // and if we subtracted "1" from min then it will be the same as max
+        System.out.println("BUSTED max = " + (max + 1));
+        System.out.println("BOOSTED min = " + (min - 1));
+
     }
 }
