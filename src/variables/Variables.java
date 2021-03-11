156324eddeb3609547450cb738eb530bf03e215e
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 20:38:45 +0530

    Added a complex expression to assign the variable

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -3,15 +3,16 @@ package variables;
 /**
  * Shows a bunch of primitive variables that we can have in Java
  * and how we can print them.
- * Version 1.1 shows how we can assign a new value to a variable as an expression.
+ * v1.1 shows how we can assign a new value to a variable as an expression.
+ * Added a complex expression in v1.2
  * @author Maverick D. Aece
- * @version 1.1
+ * @version 1.2
  */
 public class Variables {
 
     public static void main(String[] args) {
         // declares and defines(initializes) an integer variable
-        int number = 7 + 5;
+        int number = (7 + 5)  * (12 - 3);
         System.out.println("Value of the \"number\" variable = " + number);
     }
 }
