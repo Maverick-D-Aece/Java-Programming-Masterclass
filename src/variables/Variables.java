65105f857123a1b8e64abcea2eb02ec10ca34cb0
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 20:35:14 +0530

    Updated the Variables.java class to v1.1

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -3,15 +3,15 @@ package variables;
 /**
  * Shows a bunch of primitive variables that we can have in Java
  * and how we can print them.
- *
+ * Version 1.1 shows how we can assign a new value to a variable as an expression.
  * @author Maverick D. Aece
- * @version 1.0
+ * @version 1.1
  */
 public class Variables {
 
     public static void main(String[] args) {
         // declares and defines(initializes) an integer variable
-        int number = 7;
+        int number = 7 + 5;
         System.out.println("Value of the \"number\" variable = " + number);
     }
 }
