478a19ca70c41432aaee9212f334468a0375d092
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 19:53:57 +0530

    Made the Hello class to output "Hello Aece!" instead.

--- src/hello_world/Hello.java
+++ src/hello_world/Hello.java
@@ -2,14 +2,16 @@ package hello_world;
 
 /**
  * The code that breaks(?) your virginity for any programming language.
- * The afe-old, tried-and-tested Hello World!
+ * The age-old, tried-and-tested Hello World!
+ * This new version sees the edit of the string so that the program prints "Hello Aece!",
+ * kinda cool, kinda basic too though.
  *
  * @author Maverick D. Aece (Maverick-D-Aece)
- * @version 1.0
+ * @version 1.1
  */
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Aece!");
     }
 }
