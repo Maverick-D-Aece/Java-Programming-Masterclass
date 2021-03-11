c250ea97f5cd42aa6b66b9b509bc34e74177f97c
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-11 19:58:39 +0530

    Update Hello.java to print "Hello Aece!" instead.

--- src/hello_world/Hello.java
+++ src/hello_world/Hello.java
@@ -3,13 +3,15 @@ package hello_world;
 /**
  * The code that breaks(?) your virginity for any programming language.
  * The age-old, tried-and-tested Hello World!
+ * In version 1.1, the code prints "Hello Aece!" instead of "Hello World!",
+ * kinda cool but kinda basic as well.
  *
  * @author Maverick D. Aece (Maverick-D-Aece)
- * @version 1.0
+ * @version 1.1
  */
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Aece!");
     }
 }
