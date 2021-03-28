dfc36c659d0016bf0bdead4c1333fd7cd9ec077a
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-28 12:12:29 +0545

    Adding the challenge of 3E10

--- src/variables/PrimitiveVariablesChallenge.java
+++ src/variables/PrimitiveVariablesChallenge.java
@@ -1,10 +1,18 @@
 package variables;
 
 /**
- *
+ * Attempting the  challenge in 3E10
  *
  * @author Maverick D. Aece
  * @version 1.0
  */
 public class PrimitiveVariablesChallenge {
+    public static void main(String[] args) {
+        byte myByte = 10;
+        short myShort = 2345;
+        int myInt = 123456789;
+        long myLong = 50000L + 10L * (myByte + myShort + myInt);
+
+        System.out.println("Final result: " + myLong);
+    }
 }
