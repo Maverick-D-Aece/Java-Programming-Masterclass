fe67220f1b5748e8a0a6392cdca2dfb77eef08db
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-28 13:50:49 +0545

    Solution for the challenge of 3E22

---
+++ src/variables/OperatorChallenge.java
@@ -0,0 +1,20 @@
+package variables;
+
+/**
+ * Attempting the Operator Challenge of 3E22 in v1.0
+ *
+ * @author Maverick D. Aece
+ * @version 1.0
+ */
+public class OperatorChallenge {
+    public static void main(String[] args) {
+        double d1 = 20.00; // step 1
+        double d2 = 80.00; // step 2
+        double result = d1 + d2 * 100; // step 3
+        result = result % 40.00; // step 4
+
+        boolean b = (result == 0); // step 5
+        System.out.println(b); // step 6
+        if (!b) System.out.println("Got some remainder"); // step 7
+    }
+}
