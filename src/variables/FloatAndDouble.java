15563081a977f398819663252bca619d04f99ead
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-28 12:31:12 +0545

    Added code that shows the precision of floats and doubles

--- src/variables/FloatAndDouble.java
+++ src/variables/FloatAndDouble.java
@@ -3,9 +3,10 @@ package variables;
 /**
  * Added code to print the max and min value of a float and a double in v1.0
  * Added type-casting to make the float value valid in v1.1
+ * Added code that shows the precision of floats and doubles in v1.2
  *
  * @author Maverick D. Aece
- * @version 1.1
+ * @version 1.2
  */
 public class FloatAndDouble {
 
@@ -20,9 +21,12 @@ public class FloatAndDouble {
         System.out.println("Minimum Double = " + minDouble);
         System.out.println("Maximum Double = " + maxDouble);
 
-        int myInt = 5;
-        float myFloat = (float) 5.25; // this or 5.25f
-        double myDouble = 5.25;
+        int myInt = 5 / 3;
+        float myFloat = 5f / 3f; // or, 5.25f
+        double myDouble = 5.00 / 3.00;
+        System.out.println("\nInt = " + myInt);
+        System.out.println("Float = " + myFloat);
+        System.out.println("Double = " + myDouble);
     }
 
 }
